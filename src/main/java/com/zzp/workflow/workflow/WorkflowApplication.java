7b27dafe7838823159e506e5fb90551163f5239b
zhuzhengping911 <zhu_zhengping@hotmail.com>
2018-09-25 00:44:09 +0800

    代码整合

--- src/main/java/com/zzp/workflow/workflow/WorkflowApplication.java
+++ src/main/java/com/zzp/workflow/workflow/WorkflowApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkflowApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(WorkflowApplication.class, args);
     }
