049d810afae9bf4cb58b361ce1b0a51ec6bb5987
zhuzhengping911 <zhu_zhengping@hotmail.com>
2018-09-29 23:44:05 +0800

    delgate

---
+++ src/main/java/com/zzp/workflow/workflow/delgate/MyDelgate.java
@@ -0,0 +1,17 @@
+package com.zzp.workflow.workflow.delgate;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+/**
+ * Created by zhuzhengping
+ * on 2018/9/29.
+ */
+public class MyDelgate implements JavaDelegate {
+
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+
+    }
+}
